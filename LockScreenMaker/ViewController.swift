e1cd4b21d6762ea53ed5b6a46ad91e983e1aafc6
sejour <shuka.takakuma@gmail.com>
2016-09-08 23:44:33 +0900

    UIImagePickerControllerでforegroundImageとbackgroundImageを選択できるようにした

--- LockScreenMaker/ViewController.swift
+++ LockScreenMaker/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var foregroudImageView: UIImageView!
+    @IBOutlet weak var backgroundImageVIew: UIImageView!
+    weak var pickingImageView: UIImageView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func pickImageViewFromLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            let controller = UIImagePickerController()
+            controller.delegate = self
+            controller.sourceType = .PhotoLibrary
+            presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            pickingImageView?.image = image
+        }
+        pickingImageView = nil
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func didTapForegroundImageView(sender: AnyObject) {
+        pickingImageView = foregroudImageView
+        pickImageViewFromLibrary()
+    }
+    
+    @IBAction func didTapBackgroundImageView(sender: AnyObject) {
+        pickingImageView = backgroundImageVIew
+        pickImageViewFromLibrary()
+    }
+    
 }
 
