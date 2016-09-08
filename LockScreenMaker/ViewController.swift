5a41bfe238d74d75a3b33fd546481aa4bc6260e2
sejour <shuka.takakuma@gmail.com>
2016-09-08 21:56:33 +0900

    Initial Commit

---
+++ LockScreenMaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LockScreenMaker
+//
+//  Created by Shuka Takakuma on 2016/09/08.
+//  Copyright © 2016年 Shuka Takakuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
